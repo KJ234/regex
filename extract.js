b44f9d6e10a2bf9114360a3581507ee22c589940
KJ234 <komaljaved109@gmail.com>
2022-09-27 16:51:22 +0100

    email address

--- extract.js
+++ extract.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
+
 let text = fs.readFileSync("sample.txt", "utf-8")
+
 
 console.log("read", text.length, "characters.");
 
@@ -10,6 +12,7 @@ text.split("\n").forEach(
     processOneLine
 )
 
+
 console.log(`Processed ${lineCount} lines.`);
 
 // extract information from one line
@@ -19,4 +22,30 @@ function processOneLine(line) {
     lineCount++;
 
     // add code here to find emails in the line 
-}
+    
+
+    
+  
+
+    
+    let emailCount = 0;
+
+    let emailRegex =/[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/g
+    
+    text.match(emailRegex).forEach(function(email) {
+    console.log(email);
+    
+
+//   if (text.substring === '@softwire.com') console.log(text)
+
+//   text.match("@softwire.com").forEach(processEachEmail
+//   )
+
+//     console.log(`Processed ${emailCount} address`);
+
+//     function processEachEmail(line) {
+       
+//         emailCount++;
+
+  }
+    )}
