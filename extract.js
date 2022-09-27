dbc6b98b00b8f650883457426747b6433fb40358
KJ234 <komaljaved109@gmail.com>
2022-09-27 14:25:52 +0100

    first commit

---
+++ extract.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+let text = fs.readFileSync("sample.txt", "utf-8")
+
+console.log("read", text.length, "characters.");
+
+// example of information gathered
+let lineCount = 0;
+
+text.split("\n").forEach(
+    processOneLine
+)
+
+console.log(`Processed ${lineCount} lines.`);
+
+// extract information from one line
+// and update global variables
+function processOneLine(line) {
+    // example of gathering information
+    lineCount++;
+
+    // add code here to find emails in the line 
+}
